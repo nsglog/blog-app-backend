832305ee276a6903c93b4d33cda1cf63d0145b3b
unzipnav <unzip.nav@gmail.com>
2023-02-12 19:15:25 +0530

    initial commit

---
+++ src/test/java/com/scaler/blogappspringboot/BlogAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.blogappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
